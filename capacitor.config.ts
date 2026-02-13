ffb5f5b61add72067c722a06698776fa6d0470f4
Bectaly13 <dodocad13@gmail.com>
2026-02-13 05:21:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Coupe du Monde 2026',
+  webDir: 'www'
+};
+
+export default config;
